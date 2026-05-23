3d6d970aa8982a1b51b0f69a2c6f60fd38f70d26
yunajoe <yunajoe@gmail.com>
2026-05-23 14:26:38 +0900

    isValidPassword.js

--- intermediate/isValidPassword.js
+++ intermediate/isValidPassword.js
@@ -1,9 +1,12 @@
 function isValidPassword(password) {
   const alphabetRegex = /[A-Z]/g;
+  const result = password.match(alphabetRegex);
+  const condition1 = result && result.length >= 3 ? true : false;
   const digitRegex = /\d/;
   const specialLetterRegex = /[_!?*]/;
-  const condition1 = alphabetRegex.test(password.toUpperCase());
   const condition2 = digitRegex.test(password);
   const condition3 = specialLetterRegex.test(password);
   return condition1 && condition2 && condition3;
 }
+
+isValidPassword("A!B2C2");
