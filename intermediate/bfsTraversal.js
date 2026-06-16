e62c4d42386bd6e659a7b238cf9fc78a2bb1f3e5
yunajoe <yunajoe@gmail.com>
2026-06-16 21:32:51 +0900

    createPromise

---
+++ intermediate/bfsTraversal.js
@@ -0,0 +1,15 @@
+function bfsTraversal(tree) {}
+
+bfsTraversal({
+  left: {
+    left: { left: { value: 8 }, right: { value: 9 }, value: 4 },
+    right: { value: 5 },
+    value: 2,
+  },
+  right: {
+    left: { left: { value: 10 }, right: { value: 11 }, value: 6 },
+    right: { value: 7 },
+    value: 3,
+  },
+  value: 1,
+});
